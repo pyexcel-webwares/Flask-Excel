a91dd7b38179a988b82c2b5632173137b83ab128
chfw <wangc_2011@hotmail.com>
2015-02-22 16:44:07 +0000

    add download csv as zip example

---
+++ examples/zipped_csv/README.rst
@@ -0,0 +1,4 @@
+Serving a Zip file of data within a flask app
+=============================================================
+
+This application shows how to make your data as a zipped csv file
---
+++ examples/zipped_csv/example.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+from flask.ext import excel
+
+app=Flask(__name__)
+
+@app.route("/download", methods=['GET'])
+def download_file():
+    return excel.make_response_from_array([[1,2], [3, 4]], "csvz")
+
+if __name__ == "__main__":
+    app.run()
