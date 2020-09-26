41b716e7bf7a172875ffb7f8f3b6846078a12976
chfw <wangc_2011@hotmail.com>
2020-09-26 21:29:15 +0100

    :green_heart: update test code to use latest flask-sqlalchemy

--- tests/testapp.py
+++ tests/testapp.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 import pyexcel as pe
 from flask import Flask, abort, jsonify, request
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 import flask_excel as excel
 
